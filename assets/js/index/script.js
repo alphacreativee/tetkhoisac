fd2f68387ab608b7dfe56a388fa0a95c25aaff61
Taoo <hai.alphacreative@gmail.com>
2025-12-31 11:10:15 +0700

    add source

---
+++ assets/js/index/script.js
@@ -0,0 +1,32 @@
+import { preloadImages } from "../../libs/utils.js";
+("use strict");
+$ = jQuery;
+
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+};
+preloadImages("img").then(() => {
+  init();
+});
+
+let isLinkClicked = false;
+$("a").on("click", function (e) {
+  if (this.href && !this.href.match(/^#/) && !this.href.match(/^javascript:/)) {
+    isLinkClicked = true;
+  }
+});
+
+$(window).on("beforeunload", function () {
+  if (!isLinkClicked) {
+    $(window).scrollTop(0);
+  }
+  isLinkClicked = false;
+});
